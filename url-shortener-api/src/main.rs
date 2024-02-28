890ef885384e4ef7044c10ab28cac8ce4a99af76
Angus Longmore <anguslongmora@gmail.com>
2024-02-28 15:57:15 +0200

    Add Logging

--- url-shortener-api/src/main.rs
+++ url-shortener-api/src/main.rs
@@ -4,34 +4,22 @@ mod models;
 mod repository;
 
 use actix_web::{middleware, web::Data, App, HttpServer};
-use api::url_api::{hello, shorten};
+use api::url_api::{get_url, hello, shorten};
 use repository::mongodb_repo::MongoRepo;
-
-// async fn create_url_key_index(client: &Client) {
-//     let options = IndexOptions::builder().unique(true).build();
-//     let model = IndexModel::builder()
-//         .keys(doc! {"key": 1})
-//         .options(options)
-//         .build();
-//     client
-//         .database(DB_NAME)
-//         .collection::<ShortenedUrlData>(COL_NAME)
-//         .create_index(model, None)
-//         .await
-//         .expect("Creating index should succeed");
-// }
 
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     //create_url_key_index(&client).await;
     let db = MongoRepo::init().await;
     let db_data = Data::new(db);
+    pretty_env_logger::init();
     HttpServer::new(move || {
         App::new()
             .wrap(middleware::Logger::default())
             .app_data(db_data.clone())
             .service(hello)
             .service(shorten)
+            .service(get_url)
     })
     .bind(("0.0.0.0", 3000))?
     .run()
